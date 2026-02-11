30849c61ae929309a7fba4e010644dc58915b4d9
aymane-farajallah <aymanhajiqa@gmail.com>
2026-02-11 12:00:12 +0100

    project structure and switching to main

---
+++ src/test/java/projet/personalproject/PersonalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.personalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
